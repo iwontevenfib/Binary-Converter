6968cfc839fb6183084f376e89d5dba79d8bf98f
anorekarl <104476707+euphorio1@users.noreply.github.com>
2024-10-02 19:58:20 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decimal_to_Binary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+//test
+            binary();
+
+        }
+
+        static void binary()
+        {
+
+            Console.Write("\n\nEnter the number you want to convert to binary: ");
+            string input = Console.ReadLine();
+            double myNum;
+
+            while (!double.TryParse(input, out myNum))
+            {
+                Console.Write("\nPlease enter a valid number: ");
+                input = Console.ReadLine();
+
+            }
+            string binaryString = "";
+            double i;
+
+            for (i = myNum; i >= 1; i = Math.Floor(i / 2))
+            {
+                
+                if (i % 2 == 0)
+                {
+                    binaryString = "0" + binaryString;
+                }
+                else
+                {
+                    binaryString = "1" + binaryString;
+                }
+            }
+
+            Console.WriteLine("\n\n{0} in binary is {1}", myNum, binaryString);
+
+
+          newSession();
+
+
+        }
+
+
+        static void newSession()
+        {
+
+            Console.Write("\n\n\n\nDo you want to convert another number?  Type 'Y' if yes ");
+            string yes = Console.ReadLine();
+            bool loopsy = true;
+
+            while (loopsy)
+            {
+
+                if (yes == "y" || yes == "Y")
+                {
+                    binary();
+                    loopsy = false;
+                }
+                else
+                {
+                    loopsy = false;
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+
+
+
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Decimal to Binary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Decimal to Binary")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("82ea2917-d576-4dd5-a276-8e631a4cda09")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
