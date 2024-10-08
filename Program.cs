f557f265cdbb38882ae750f7c3920377994ffc88
Karl Anore <anorekarlxz@gmail.com>
2024-10-08 21:39:58 +0800

    em den

--- Program.cs
+++ Program.cs
@@ -8,14 +8,86 @@ namespace Decimal_to_Binary
 {
     internal class Program
     {
+         // boolean for newsession
+        static bool optionHandler;
+
         static void Main(string[] args)
         {
-//test
-            binary();
+
+          // Main
+
+            string optionTest;
+
+            Console.WriteLine("\t\t\t\tWelcome to the Lanes!");
+            Console.Write("\nWhat do you want to convert? type (1) 'Binary To Decimal' | (2) 'Decimal to Binary': ");
+            optionTest = Console.ReadLine();
+
+            if (optionTest == "1")
+            {
+                binaryToDecimal();
+                optionHandler = false;
+            }
+            else if (optionTest == "2")
+            {
+                decimalToBinary();
+                optionHandler = true;
+            }
+
+
+
 
         }
 
-        static void binary()
+       // BINARY TO DECIMAL
+
+        static void binaryToDecimal()
+        {
+
+
+
+            string myNum;
+            int power = -1;
+
+            Console.WriteLine("\nNote: Binary is only 1's and 0's.");
+            Console.Write("Enter the binary number you want to convert to decimal: ");
+
+            myNum = Console.ReadLine();
+
+            double myDecimal = 0;
+
+
+
+
+            for (int i = myNum.Length - 1; i >= 0; i--)
+            {
+
+                power++;
+
+                if (myNum[i] == '1')
+                {
+                    myDecimal = myDecimal + Math.Pow(2, power);
+
+                }
+                else
+                {
+                    myDecimal = myDecimal + 0;
+                }
+
+            }
+
+            // OUTPUT
+
+            Console.WriteLine("The decimal number is: {0}", myDecimal);
+
+            // New Session
+
+            newSession();
+
+        }
+
+        // DECIMAL TO BINARY
+
+        static void decimalToBinary()
         {
 
             Console.Write("\n\nEnter the number you want to convert to binary: ");
@@ -33,7 +105,7 @@ namespace Decimal_to_Binary
 
             for (i = myNum; i >= 1; i = Math.Floor(i / 2))
             {
-                
+
                 if (i % 2 == 0)
                 {
                     binaryString = "0" + binaryString;
@@ -47,7 +119,7 @@ namespace Decimal_to_Binary
             Console.WriteLine("\n\n{0} in binary is {1}", myNum, binaryString);
 
 
-          newSession();
+            newSession();
 
 
         }
@@ -56,27 +128,56 @@ namespace Decimal_to_Binary
         static void newSession()
         {
 
-            Console.Write("\n\n\n\nDo you want to convert another number?  Type 'Y' if yes ");
-            string yes = Console.ReadLine();
-            bool loopsy = true;
-
-            while (loopsy)
+            if (optionHandler)
             {
+                Console.Write("\n\n\n\nDo you want to convert another number?  Type 'Y' if yes ");
+                string yes = Console.ReadLine();
+                bool loopsy = true;
 
-                if (yes == "y" || yes == "Y")
+                while (loopsy)
                 {
-                    binary();
-                    loopsy = false;
+
+                    if (yes == "y" || yes == "Y")
+                    {
+                        decimalToBinary();
+                        loopsy = false;
+                    }
+                    else
+                    {
+                        loopsy = false;
+                    }
                 }
-                else
+
+                Console.ReadKey();
+
+            }
+            else
+            {
+                Console.Write("\n\n\n\nDo you want to convert another number?  Type 'Y' if yes ");
+                string yes = Console.ReadLine();
+                bool loopsy = true;
+
+                while (loopsy)
                 {
-                    loopsy = false;
+
+                    if (yes == "y" || yes == "Y")
+                    {
+                        binaryToDecimal();
+                        loopsy = false;
+                    }
+                    else
+                    {
+                        loopsy = false;
+                    }
                 }
+
+                Console.ReadKey();
+
             }
 
-            Console.ReadKey();
 
         }
+
 
 
 
